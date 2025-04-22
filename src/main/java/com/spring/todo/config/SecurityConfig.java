44930c8291857681abdf58c22229034d6d03f421
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-04-23 00:45:01 +0200

    Add Security Config to limit requests to gateway only

---
+++ src/main/java/com/spring/todo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.spring.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/items/**").permitAll()
+                        .anyRequest().denyAll()
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
+
