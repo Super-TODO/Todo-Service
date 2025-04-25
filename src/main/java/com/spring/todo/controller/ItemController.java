ece690a6b17f92c64cb352779e750d9567faef25
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-04-25 05:10:27 +0300

    Add CrossOrigin

--- src/main/java/com/spring/todo/controller/ItemController.java
+++ src/main/java/com/spring/todo/controller/ItemController.java
@@ -22,6 +22,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/items")
+@CrossOrigin(origins = "*")
 public class ItemController {
 
     private final ItemService itemService;
